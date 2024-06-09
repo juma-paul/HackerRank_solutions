a414056652a8425e7a7cf14e932e439c7b53863c
Juma Paul <51334670+juma-paul@users.noreply.github.com>
2024-06-09 14:05:07 -0700

    Create leap_year.py
    Adding a new repository, with a function for determining if a year is a leap year or not.

---
+++ leap_year.py
@@ -0,0 +1,14 @@
+def leap_year(year) -> bool:
+  leap = False
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        leap = True
+      else:
+        leap = False
+    else:
+      leap = True
+  else:
+    leap = False
+
+  return leap
