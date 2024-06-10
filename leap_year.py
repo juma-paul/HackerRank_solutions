d4497a808dbe3d56b234ba45acad869a54a9a705
Juma Paul <51334670+juma-paul@users.noreply.github.com>
2024-06-10 00:44:36 -0700

    Docstring
    Added docstrings to the function leap_year and the main method

--- leap_year.py
+++ leap_year.py
@@ -1,4 +1,14 @@
 def leap_year(year) -> bool:
+  """
+  A function to determine if a given year is a leap year.
+
+  Params:
+    year(int): the year that the user inputs.
+  
+  Returns:
+      bool: True or False.
+  """
+  
   leap = False
   if year % 4 == 0:
     if year % 100 == 0:
@@ -14,6 +24,10 @@ def leap_year(year) -> bool:
   return leap
 
 def main():
+  """
+  A main method for testing the function leap_year.
+  """
+  
   year = int(input("Enter a year, to determine if it's a leap year or not: "))
   res = leap_year(year)
   if res == True:
