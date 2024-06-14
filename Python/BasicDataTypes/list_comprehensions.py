d4b805b05c7885ae721da9df60f31ac35f306ed8
juma <raisben7@gmail.com>
2024-06-14 00:13:34 -0700

    Add docstrings to the methods

--- Python/BasicDataTypes/list_comprehensions.py
+++ Python/BasicDataTypes/list_comprehensions.py
@@ -1,9 +1,23 @@
 def possible_cordinates(x, y, z, n):
+    """
+    A function to print all possible coordinates on a 3D grid
+
+    params:
+        x(int) - first cuboid dimension 
+        y(int) - second cuboid dimension 
+        z(int) - third cuboid dimension 
+        n(int) - integer to ensure i + j + k != n
+    """
+    
     cuboid = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if (i + j + k) != n]
     return cuboid
 
 def main():
-    print("Please enter four integers to print dimensions of the cuboid: ")
+    """
+    A main method for testing the function possiblecordinates()
+    """
+
+    print("\nPlease enter four integers to print dimensions of the cuboid: ")
     x = int(input("x: ")) # x: 1
     y = int(input("y: ")) # y: 1
     z = int(input("z: ")) # z: 1
