02fc42b6f8ff5890d6041796fce96ad3c5e343fa
juma <raisben7@gmail.com>
2024-06-20 15:22:24 -0700

    Implement solution for the 'find angle MBC' problem

---
+++ Python/Math/2_find_angle_mbc.py
@@ -0,0 +1,32 @@
+import math
+
+def angle_of_triangle(AB: int, BC: int) -> int:
+    """
+    Calculate the angle of a triangle using the lengths of the adjacent (AB) and opposite (BC) sides.
+
+    Args:
+        AB (int): Length of the adjacent side.
+        BC (int): Length of the opposite side.
+
+    Returns:
+        int: The angle MBC in degrees.
+    """
+    
+    angle_MBC = math.atan2(AB, BC)
+    MBC = round(math.degrees(angle_MBC))
+
+    return MBC
+
+def main():
+    """
+    Main function to calculate and print the angle of a triangle.
+    """
+
+    opp = int(input(f'\nEnter length of opposite side of triangle: '))
+    adj = int(input(f'\nEnter length of adjacent side of triangle: '))
+
+    res = angle_of_triangle(opp, adj)
+    print(f'\n<MBC = {res}°\n')
+
+if __name__ == '__main__':
+    main()
