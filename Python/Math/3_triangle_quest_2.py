fcfa0bc6ca1c0d0cf533a5932eed02219e2295b5
juma <raisben7@gmail.com>
2024-06-22 10:35:47 -0700

    Break program into small steps and add comments

--- Python/Math/3_triangle_quest_2.py
+++ Python/Math/3_triangle_quest_2.py
@@ -2,8 +2,11 @@ def main():
     """
     Generates and prints a palindromic triangle of a given size.
     """
-    for i in range(1, int(input("Enter integer to represent size of triangle: ")) + 1):
-        print((10 ** i // 9) ** 2)
+
+    n = int(input("Enter integer to represent size of triangle: "))
+    for i in range(1, n + 1):
+        k = (10 ** i // 9) # create a number composed entirely of 1s
+        print(k ** 2) # create a palindromic number
 
 if __name__ == '__main__':
     main()
