02384e427e42a61ad2daf26b7448dbf0940a2089
juma <raisben7@gmail.com>
2024-06-22 10:44:54 -0700

    Modify the docstring and print statements to be smaller

--- Python/Math/4_mod_divmod.py
+++ Python/Math/4_mod_divmod.py
@@ -1,17 +1,13 @@
 def main():
     """
-    Reads two integers from input and prints:
-        - The integer division of the first integer by the second.
-        - The remainder of the first integer divided by the second.
-        - The result of divmod on the two integers as a tuple.
+    Reads two integers and prints their integer division, remainder, and divmod result.
     """
 
     a = int(input("Enter integer a: "))
     b = int(input("Enter integer b: "))
     quotient, remainder = divmod(a, b)
-    print(quotient)
-    print(remainder)
-    print((quotient, remainder))
+    
+    print(f"\n{quotient}\n{remainder}\n{(quotient, remainder)}")
 
 if __name__ == '__main__':
     main()
