57cccca7fa221814b1878994be1578aad68d4391
juma <raisben7@gmail.com>
2024-06-22 12:32:57 -0700

    Implement solution for permutations problem

---
+++ Python/Itertools/2_itertools_permutations.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+def main():
+    """
+    Create k possible  permutations of a given string S.
+    """
+    
+    S, k = input("Eneter space-separated string number of permutations needed: ").split()
+
+    k = int(k)
+
+    permut_S = permutations(sorted(S), k)
+
+    for item in permut_S:
+        item = "".join(item)
+        print(item)
+
+if __name__ == "__main__":
+    main()
