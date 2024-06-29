9e7c6cb1f498fa6c5dd1cf219ea429167a50addf
juma <raisben7@gmail.com>
2024-06-28 18:16:27 -0700

    Implement methods for operations in complex class

--- Python/Classes/1_complex_numbers.py
+++ Python/Classes/1_complex_numbers.py
@@ -3,26 +3,40 @@ class Complex:
         self.real = real
         self.imaginary = imaginary
     
-    def __add__(self, num: float) -> complex:
-        ...
+    def __add__(self, num: 'Complex') -> 'Complex':
+        real = self.real + num.real
+        imag = self.imaginary + num.imaginary
+        return Complex(real, imag)
+
     
-    def __sub__(self, num: float) -> complex:
-        ...
+    def __sub__(self, num: 'Complex') -> 'Complex':
+        real = self.real - num.real
+        imag = self.imaginary - num.imaginary
+        return Complex(real, imag)
     
-    def __mul__(self, num: float) -> complex:
-        ...
+    def __mul__(self, num: 'Complex') -> 'Complex':
+        real = self.real * num.real - self.imaginary * num.imaginary
+        imag = self.real * num.imaginary + self.imaginary * num.real
+        return Complex(real, imag)
     
-    def __truediv__(self, num: float) -> complex:
-        ...
-    
-    def __str__(self) -> str:
-        ...
+    def __truediv__(self, num: 'Complex') -> 'Complex':
+        conjugate = Complex(num.real, -num.imaginary)
+        numerator = self * conjugate
+        denomenator = num.real ** 2 + num.imaginary ** 2
+        real = numerator.real / denomenator
+        imag = numerator.imaginary / denomenator
+        return Complex(real, imag)
     
     def mod(self) -> complex:
-        ...
+        real = (self.real ** 2 + self.imaginary ** 2) ** 0.5
+        imag = 0
+        return Complex(real, imag)
+    
+    def __str__(self) -> str:
+        pass
 
 def main():
-    ...
+    pass
 
 if __name__ == '__main__':
     main()
