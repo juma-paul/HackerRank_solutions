6e320b277ab9a0dc99f555dad4b872c3f9a4b2c1
juma <raisben7@gmail.com>
2024-07-05 14:37:41 -0700

    Add docstrings to the functions

--- Python/Strings/8_text_wrap.py
+++ Python/Strings/8_text_wrap.py
@@ -1,11 +1,27 @@
 import textwrap
 
 def wrap(string: str, max_width: int) -> str:
+    """
+    Wraps the input string into lines of specified maximum width.
+
+    Args:
+        string (str): The input string to be wrapped.
+        max_width (int): The maximum width of each line.
+
+    Returns:
+        str: The wrapped string with lines separated by newline characters.
+    """
+
     wrapped_text = textwrap.wrap(string, max_width)
     return '\n'.join(wrapped_text)
 
 def main():
-    string, max_width = input('\nEnter string: '), int(input('\nEnter maximum length to wrap: '))
+    """
+    Main function to execute the wrapping of input string based on user input for maximum width.
+    """
+    
+    string = input('\nEnter string: ')
+    max_width = int(input('\nEnter maximum length to wrap: '))
     res = wrap(string, max_width)
     print(f'\n{res}\n')
 
